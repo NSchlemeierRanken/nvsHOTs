fcab267f025cacc10b3ddcddf0cf08f3bd4bb066
Nathan Schlemeier <Nathan_Schlemeier@insideranken.org>
2024-01-19 20:57:58 -0600

    HOT 1

---
+++ HOTs/HOT102/HOT102/Average.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOT102
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string intOne;
+            //string intTwo;
+            //string intThree;
+            //double one;
+            //double two;
+            //double three;
+
+            Console.Write("Enter your first number: ");
+            double one = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your second number: ");
+            double two = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your third number: ");
+            double three = Convert.ToDouble(Console.ReadLine());
+
+            double sum = one + two + three;
+            Console.WriteLine("The sum of those three numbers is " + sum);
+
+            double average = sum / 3;
+            Console.WriteLine("The average of those three numbers is " + average);
+        }
+    }
+}
---
+++ HOTs/HOT103/HOT103/TShirts.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOT103
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string address;
+            string city;
+            string state;
+            string zip;
+            string tshirts;
+            double amount;
+
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Street Address: ");
+            address = Console.ReadLine();
+            Console.Write("City: ");
+            city = Console.ReadLine();
+            Console.Write("State: ");
+            state = Console.ReadLine();
+            Console.Write("Zip: ");
+            zip = Console.ReadLine();
+            Console.Write("How many T-Shirts do you want to order? ");
+            tshirts = Console.ReadLine();
+            amount = Convert.ToDouble(tshirts);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            double total = amount * 14.99;
+            double tax = total * 0.08;
+            double due = tax + total;
+
+            Console.WriteLine("Receipt for: ");
+            Console.WriteLine(name);
+            Console.WriteLine(address);
+            Console.WriteLine($"{city}, {state} {zip}");
+            Console.WriteLine();
+            Console.WriteLine(tshirts + " T-Shirts ordered @ 14.99 each");
+            Console.WriteLine("Total: $" + total.ToString("F2"));
+            Console.WriteLine("Tax:   $" + tax.ToString("F2"));
+            Console.WriteLine("---------------");
+            Console.WriteLine("Due:   $" + due.ToString("F2"));
+        }
+    }
+}
